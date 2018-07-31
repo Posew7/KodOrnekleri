a3d55535432168fe62c7785a06326eeaf31be3a4
Yunus <Yunus@DESKTOP-F8LKHQI>
2018-07-31 11:35:35 +0300

    Verilen bir String dizisini, ters sırada (sondan ba şa do ğru) listeleyen Java programını yazınız.

---
+++ src/com/posew7/stringdizi/StringDizi.java
@@ -0,0 +1,16 @@
+package com.posew7.stringdizi;
+
+public class StringDizi {
+
+    public static void main(String[] args) {
+
+        String dizi[] = {"ford", "audi", "mercedes", "bmw", "subaru"};
+
+        for (int i = 4; i >= 0; i--) {
+
+            System.out.println(dizi[i]);
+
+        }
+    }
+
+}
